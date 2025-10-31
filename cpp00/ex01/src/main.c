e06618eb9144970ed8d712558b7a8f3033362237
ysn-elg <yassine.elguaddar99@gmail.com>
2025-10-31 11:06:52 +0100

    handle ctrl + D

--- cpp00/ex01/src/main.c
+++ cpp00/ex01/src/main.c
@@ -1,13 +1,27 @@
 //#include "PhoneBook.hpp"
 #include <iostream>
-int main() {
+
+std::string trim(const std::string& str, const std::string& set)
+{
+    size_t start = str.find_first_not_of(set);
+    if (start == std::string::npos)
+        return "";
+
+    size_t end = str.find_last_not_of(set);
+    return str.substr(start, end - start + 1);
+}
+
+
+int main()
+{
     std::string command;
 
-    std::cout <<"Available commands: ADD, SEARCH, EXIT.\n";
     while(true)
     {
         std::cout << "Enter a command: ";
-        std::getline(std::cin, command);
+        if (!std::getline(std::cin, command))
+            break;
+        command = trim(command, " \t\v\f\r");
         if (command == "ADD")
             std::cout << "you have selected 'ADD'\n";
         else if (command == "SEARCH")
@@ -17,9 +31,7 @@ int main() {
         else if (command.empty())
             continue;
         else
-        {
-            std::cout << "  Invalid command! try:  ADD, SEARCH or EXIT.\n";
-        }
+            std::cout << command << ": invalid command! try 'ADD', 'SEARCH' or 'EXIT'.\n";
     }
     return 0;
 }
