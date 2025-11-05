4bebcfb4de65591ca8c78520550e571311f77a7e
ysn-elg <yassine.elguaddar99@gmail.com>
2025-11-05 15:11:16 +0100

    quick edit

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
 int main(int ac, char **av)
 {
     if (ac == 1) {
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
         return 1;
     }
     for (int i = 1; av[i]; i++)
         for (int j = 0; av[i][j]; j++)
-            cout << (char)toupper(av[i][j]);
-    cout << endl;
+            std::cout << (char)toupper(av[i][j]);
+    std::cout << endl;
 return 0;
 }
--- cpp00/ex01/src/PhoneBook.cpp
+++ cpp00/ex01/src/PhoneBook.cpp
@@ -1,6 +1,6 @@
 #include "PhoneBook.hpp"
 
-int setData(const std::string msg, void (*func)(const std::string))
+int setData(const std::string& msg, const std::function<void(const std::string&)>& func) // check parameter again
 {
     std::string tmp;
 
@@ -22,11 +22,34 @@ void    PhoneBook::addContact()
     std::string tmp;
     
     storedCount = (storedCount + 1) % 8;
-    if (setData("First name: ", contacts[storedCount].setFirstName)
-        || setData("Last name: ", contacts[storedCount].setLastName)
-        || setData("Nick name: ", contacts[storedCount].setNickName)
-        || setData("Phone number: ", contacts[storedCount].setPhoneNumber)
-        || setData("Darkest secret: ", contacts[storedCount].setDarkestSecret)) 
+    if (setData("   First name: ", contacts[storedCount].setFirstName) \\ check typing
+    ||  setData("   Last name: ", contacts[storedCount].setLastName)
+    ||  setData("   Nick name: ", contacts[storedCount].setNickName)
+    ||  setData("   Phone number: ", contacts[storedCount].setPhoneNumber)
+    ||  setData("   Darkest secret: ", contacts[storedCount].setDarkestSecret)) 
             return 1;
     return 0;
+}
+
+std::string    formatField(const std::string& str)
+{
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";
+    else
+        return str;
+}
+
+void    displayContact(Contact C)
+{
+    
+}
+
+void    PhoneBook::searchContact()
+{
+    if (storedCount < 0)
+    {
+        std::cout "PhoneBook is empty" << endl;
+        return ; //todo: 
+    }
+    
 }
