fbe2a2ea9733bad84fa1c0046b8460be4f2cb8c0
ysn-elg <yassine.elguaddar99@gmail.com>
2025-11-13 11:38:39 +0100

    few changes on account.cpp

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,8 +1,46 @@
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+    : _accountIndex(_nbAccounts),
+      _amount(inial_deposit),
+      _nbDeposits(0),
+      _nbWithdrawals(0)
+{
+        
+}
+
+void    Account::makeDeposit( int deposit )
+{
+    _amount += deposit; // should we check if deposit < 0 ?
+    _nbDeposite++;
+}
+
+bool    Account::makeWithdrawal( int withdrawal )
+{
+    if (_amount < deposit)
+    {
+        std::cout << "****" << std::endl;
+        return false;
+    }
+    _amount -= withdrawal;
+    _nbWithdrawal++;
+    return true;
+}
+
+int     Account::checkAmount( void )        { return _amount; }
+
+void    Account::displayStatus( void )
+{
+    _displaTimestamp();
+    std::cout << "index:" << _accountIndex;
+    std::cout << ";amount:" << checkAmount();
+    std::cout << ";deposit:" << _nbDeposits;
+    std::cout << ";withdraw:" << _nbWithdrawals; << std::endl ;
+}
+
+//Global
 int Account::getNbAccounts( void )      { return _accountIndex; }
-
-static int	getTotalAmount( void )      { return _totalAmount; }
-
-static int	getNbDeposits( void )       { return _totalNbDeposits; }
-
-static int	getNbWithdrawals( void )    { return _totalNbWithdrawals; }
+int	Account::getTotalAmount( void )     { return _totalAmount; }
+int	Account::getNbDeposits( void )      { return _totalNbDeposits; }
+int	Account::getNbWithdrawals( void )   { return _totalNbWithdrawals; }
 
