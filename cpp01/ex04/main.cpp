f48e91b52a10abbe68e92f3217f30059ca8d9b89
ysn-elg <yassine.elguaddar99@gmail.com>
2025-11-19 18:24:20 +0100

    cpp01 ex04 without makefile

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main (int ac, char *av[])
 {
@@ -8,9 +9,9 @@ int main (int ac, char *av[])
         return 1;
     }
 
-    std::string fileName = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
+    std::string fileName = av[1];
+    std::string s1 = av[2];
+    std::string s2 = av[3];
     if (s1.empty()) {
         std::cerr << "Error: s1 must not be empty\n";
         return 1;
@@ -23,23 +24,30 @@ int main (int ac, char *av[])
         return 1;
     }
 
-
     std::stringstream tmp;
     tmp << file.rdbuf();
     std::string buff = tmp.str();
     file.close();
-    str::size_t count = 0;
-    
-    std::size_t count = 0;
-    std::size_t p = 0;
 
+    std::string result;
+    size_t pos = 0;
+    size_t found;
+
+    while ((found = buff.find(s1, pos)) != std::string::npos) {
+        result.append(buff, pos, found - pos);
+        result.append(s2);
+        pos = found + s1.size();
+    }
+    result += buff.substr(pos);
+    
     std::ofstream outFile((fileName + ".replace").c_str());
     if (!outFile.is_open())
     {
         std::cerr << "Error creating outFile\n";
         return 1;
     }
-    
+    outFile << result;
+    outFile.close();
     return 0;
 }
 
