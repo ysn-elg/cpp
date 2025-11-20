6e87a4e42256ad573b1d3a0e12d9582b2688b3f0
ysn-elg <yassine.elguaddar99@gmail.com>
2025-11-20 15:22:58 +0100

    cpp01 ex06

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,40 @@
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+	std::cout   << "[ DEBUG ]\n"
+                << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n" << std::endl;
+}
+void Harl::info(void)
+{
+	std::cout   << "[ INFO ]\n" 
+                << "I cannot believe adding extra bacon costs more money.\n"
+                << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout   << "[ WARNING ]\n"
+                << "I think I deserve to have some extra bacon for free.\n"
+                << "I’ve been coming for years whereas you started working here since last month.\n" << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout   << "[ ERROR ]\n"
+                << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*funcs[])() = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+
+    for (int i = 0; i < 4; i++) {
+        if (level == levels[i]) {
+            (this->*funcs[i])();
+            return;
+        }
+    }
+}
+
---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,30 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    Harl harl;
+
+    if (ac != 2)
+    {
+        std::cerr << av[0] << " <level>\n"
+                  << "Levels: (1) DEBUG, (2) INFO, (3) WARNING, (4) ERROR" << std::endl;
+        return 1;
+    }
+    std::string level = av[1];
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    int start = 0;
+    while (start < 4 && levels[start] != level)
+        start++;
+
+    if (start == 4)
+    {
+        std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            return 1;
+    }
+    for (int i = start; i < 4; i++)
+        harl.complain(levels[i]);
+    return 0;
+}
+
