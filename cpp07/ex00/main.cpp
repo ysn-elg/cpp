4c3e39d6ceb7f57f6d31bc98ee691c825148da25
ysn-elg <yassine.elguaddar99@gmail.com>
2026-01-31 16:08:38 +0100

    subject main

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -2,21 +2,24 @@
 #include <string>
 #include <iostream>
 
-int main() {
-    int a = -2, b = 1;
-    
-    ::swap(a, b);
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
-
-    std::string s1 = "ABCDEF";
-    std::string s2 = "ABCDEZ";
-
-    ::swap(s1, s2);
-    std::cout << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
-    std::cout << "min(s1, s2) = " << ::min(s1, s2) << std::endl;
-    std::cout << "max(s1, s2) = " << ::max(s1, s2) << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     
     return 0;
 }
