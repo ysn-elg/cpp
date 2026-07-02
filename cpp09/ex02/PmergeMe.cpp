909e8a2c9c5daf607eddd9f99ffad819e8d222f6
ysn-elg <yassine.elguaddar99@gmail.com>
2026-07-02 12:14:06 +0100

    start implementation of algo

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,10 +1,14 @@
 #include "PmergeMe.hpp"
+#include <bits/types/struct_timeval.h>
 #include <cctype>
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
 #include <string>
+#include <sys/time.h>
+#include <utility>
+#include <vector>
 
 PmergeMe::PmergeMe() {}
 
@@ -70,9 +74,61 @@ void PmergeMe::displayAfter() const {
     std::cout << std::endl;
 }
 
+void PmergeMe::sortVector() {
+    timeval start;
+    gettimeofday(&start, NULL);
+
+    mergeInsertSort(_vec, 0, _vec.size() - 1);
+
+    timeval end;
+    gettimeofday(&end, NULL);
+
+    long duration = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_sec - start.tv_sec);
+    
+    std::cout << "Time to process a range of " << _vec.size() 
+              << " elements with std::vector : " << duration 
+              << " us" << std::endl;
+}
+
+void PmergeMe::mergeInsertSort(std::vector<int> &arr, int left, int right) {
+    if (left >= right)
+        return ;
+
+    std::vector<std::pair<int, int> > pairs;
+
+    for (int i = left; i < right; i += 2) { // i = 0,1; i = 2,3 #3
+        int a = arr[i];
+        int b = arr[i + 1];
+        
+        if (a < b)
+            pairs.push_back(std::make_pair(a, b));
+        else
+            pairs.push_back(std::make_pair(b, a));
+    }
+    int oddElement = -1;
+    if (i == right)
+        oddElement = arr[right];
+
+    std::vector<int> larger;
+    for (std::size_t i = 0; i < pairs.size(); ++i)
+        larger.push_back(pairs[i].second);
+    
+    // if (oddElement == -1)
+    //    chi haja;
+    
+    if (larger.size() > 1)
+        mergeInsertSort(larger, 0, larger.size() - 1);
+
+    std::vector<int> mainChain = larger;
+
+    for (std::size_t i = 0; i < pairs.size(); ++i)
+}
+
 void PmergeMe::run(int ac, char** av) {
     parseArguments(ac, av);
     displayBefore();
+    
+    sortVector();
     // TODO: sort using algo ...
     displayAfter();
 }
